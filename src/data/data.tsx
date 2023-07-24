886caab8f7fbe5bd676c1b7b391ee0fd5ed133a9
Christiano Barbosa <kaelbard@gmail.com>
2023-07-24 14:50:01 -0300

    update

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -44,8 +44,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Defuse',
+  description: "Construa a autoridade da sua marca através do seu posicionamento digital!",
 };
 
 /**
